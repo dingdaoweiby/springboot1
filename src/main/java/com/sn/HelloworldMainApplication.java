64a29915a4fe69a693b14784770ffca68d952c16
dingdaoweiby <dingdaoweiby@gmail.com>
2019-11-14 08:12:45 -0500

    springboot first section

---
+++ src/main/java/com/sn/HelloworldMainApplication.java
@@ -0,0 +1,13 @@
+package com.sn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldMainApplication.class, args);
+    }
+
+}
